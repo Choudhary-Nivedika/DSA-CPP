ebd60bc097228a9ef2f0e10545c2377dbd88731e
NivedikaChoudhary <nivedikachoudhary2@gmail.com>
2025-10-17 22:46:37 +0530

    initial commit

---
+++ L05_loops/Day06/Q1_print_numbers_1_to_n.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //for loop
+    int num;
+    for(num = 1; num<=5; num++){
+
+        cout<<num<<endl;
+    }
+    cout<<"Last value of num "<<num<<endl;
+    
+    return 0;
+}
---
+++ L05_loops/Day06/Q2_print_consistency_5_times.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    // for(int i = 0; i<5; i++){
+    for(int i = 1; i<=5; i++){
+        cout<<"Consistency"<<endl;
+    }
+
+    return 0;
+}

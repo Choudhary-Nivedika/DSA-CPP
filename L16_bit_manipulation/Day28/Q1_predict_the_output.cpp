e6a7291b8339aa86b363b8e0d18e1e8b354902c9
NivedikaChoudhary <nivedikachoudhary2@gmail.com>
2025-11-17 23:53:21 +0530

    initial commit

---
+++ L16_bit_manipulation/Day28/Q1_predict_the_output.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //predict the output for ~4
+    cout<<(~4)<<endl;
+
+    //Predict the output for 8 >> 1
+    cout<<(8 >> 1)<<endl;
+
+    return 0;
+}

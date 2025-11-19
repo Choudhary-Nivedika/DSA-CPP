44125031f80cb02ac5b449d8a1bed1265ddecfe5
NivedikaChoudhary <nivedikachoudhary2@gmail.com>
2025-11-19 23:57:51 +0530

    initial commit

---
+++ L16_bit_manipulation/Day28/Q6_check_power_of_2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+bool isPowerOf2(int num){
+    if(!(num & (num -1))){
+        return true;
+    }
+    else return false;
+}
+
+int main(){
+    cout<<isPowerOf2(4)<<endl;
+    cout<<isPowerOf2(16)<<endl;
+    cout<<isPowerOf2(13)<<endl;
+    cout<<isPowerOf2(1)<<endl;
+    
+    return 0;
+}

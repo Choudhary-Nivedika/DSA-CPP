05b60f8ef28345505d1fa73c525e419d76ad7f47
NivedikaChoudhary <nivedikachoudhary2@gmail.com>
2026-01-18 23:25:43 +0530

    initial commit

---
+++ L25_linked_list_part1/Day46/Q1_find_and_remove_nth_node_from_end.cpp
@@ -0,0 +1,218 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node* next;
+
+        Node(int val){
+            data = val;
+            next = NULL;
+        }
+
+        ~Node(){
+
+            if(next != NULL){
+                
+                delete next;
+                next = NULL;
+            }
+        }
+};
+
+class List {
+    Node* head;
+    Node* tail;
+    
+    public:
+        List(){
+            head = NULL;
+            tail = NULL;
+        }
+
+        ~List(){
+
+            if(head != NULL){
+                delete head;
+                head = NULL;
+            }
+        }
+
+        void push_front(int val){
+            Node* newNode = new Node(val); //dynamic
+            // Node* newNode(val); //static
+
+            if(head == NULL){
+                head = tail = newNode;
+            } 
+            else{
+                newNode->next = head;
+                head = newNode;
+            }
+        }
+
+        void push_back(int val){
+            Node* newNode = new Node(val);
+
+            if(head == NULL){
+                head = tail = newNode;
+            } else{
+                tail->next = newNode;
+                tail = newNode;
+            }
+        }
+
+        void printList(){
+            Node* temp = head;
+
+            while(temp != NULL){
+                cout<<temp->data<<" -> ";
+                temp = temp->next;
+            }
+            cout<<"NULL"<<endl;
+        }
+
+        void insert(int val, int pos){
+            Node* newNode = new Node(val);
+
+            Node* temp = head;
+            for(int i=0; i<pos-1; i++){
+                if(temp == NULL){
+                    cout<<"position is INVALID"<<endl;
+                    return;
+                }
+                temp = temp->next;
+            }
+
+            //temp is now at pos-1 i.e. prevv/left
+            newNode->next = temp->next;
+            temp->next = newNode;
+        }
+
+        void pop_front(){
+            if(head ==  NULL){
+                cout<<"LL is empty"<<endl;
+                return;
+            }
+            Node* temp = head;
+            head = head->next;
+
+            temp->next = NULL;
+            delete temp;
+
+        }
+
+        void pop_back(){
+            if(head == NULL){
+                cout<<"LL is empty"<<endl;
+                return;
+            }
+            Node* temp = head;
+            while(temp->next->next != NULL){
+                temp = temp->next;
+            }
+
+            temp->next = NULL; //temp = tail's prev
+            delete tail;
+            tail = temp;
+        }
+
+        int searchItr(int key){
+            Node* temp = head;
+            int idx = 0;
+
+            while(temp != NULL){
+                if(temp->data == key){
+                    return idx;
+                }
+
+                temp = temp->next;
+                idx++;
+            }
+
+            return -1;
+
+        }
+
+        int helper(Node* h, int key){
+            if(h == NULL){
+                return -1;
+            }
+
+            if(h->data == key){
+                return 0;
+            }
+
+            int idx = helper(h->next, key);
+            if(idx == -1){
+                return -1;
+            }
+
+            return idx + 1;
+        }
+
+        int searchRec(int key){
+            return helper(head, key);
+        }
+
+        void reverse(){
+            Node* curr = head;
+            Node* prev = NULL;
+
+            while(curr != NULL){
+                Node* next = curr->next;
+                curr->next = prev;
+
+                //updations for next itr
+                prev = curr;
+                curr = next;
+            }
+
+            head = prev;
+        }
+
+        int getSize(){
+            int sz = 0;
+            Node* temp = head;
+
+            while(temp != NULL){
+                temp = temp->next;
+                sz++;
+            }
+            return sz;
+        }
+
+        void removeNth(int n){  //TC-O(n)  ;SC->O(1)
+            int size = getSize();
+            Node* prev = head;
+
+            for(int i=1; i<(size-n); i++){ //i=size-n => deletion node prev
+                prev = prev->next;
+            }
+
+            Node* toDel = prev->next;
+            cout<<"going to delete : "<<toDel->data <<endl;
+            prev->next = prev->next->next;  
+            
+        }
+};
+
+int main(){
+
+    List ll;
+    
+    ll.push_front(5);
+    ll.push_front(4);
+    ll.push_front(3);
+    ll.push_front(2);
+    ll.push_front(1);
+
+    ll.printList();
+
+    ll.removeNth(2);
+    ll.printList();
+
+ 
+    return 0;
+}

85bd630df992e4137388bcd943ddc24d4867c0ef
NivedikaChoudhary <nivedikachoudhary2@gmail.com>
2026-02-15 20:12:29 +0530

    initial commit

---
+++ L27_stacks_part1/Dayy50/Q2_stack_using_vector_and_class_template.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <string> 
+using namespace std;
+
+//create Stack using vector
+
+template<class T>
+class Stack{
+    vector<T> vec;
+
+    public:
+        void push(T val){        //O(1)
+            vec.push_back(val);
+        }
+
+        void pop(){   //O(1)
+            if(isEmpty()){
+                cout<<"Stack is empty"<<endl;
+                return;
+            }
+            vec.pop_back();
+        }
+
+        T top(){   //O(1)
+            // if(isEmpty()){
+            //     cout<<"Stack is empty"<<endl;
+            //     return -1;
+            // }
+            int lastIdx = vec.size()-1;
+            return vec[lastIdx];
+        }
+
+        bool isEmpty(){
+            return vec.size() == 0;
+        }
+};
+
+int main(){
+
+    Stack<char> s;
+    
+    
+    s.push('c');
+    s.push('b');
+    s.push('a');
+
+    while(!s.isEmpty()){
+        cout<<s.top()<<" ";
+        s.pop();
+    }
+    cout<<endl;
+    
+    return 0;
+}

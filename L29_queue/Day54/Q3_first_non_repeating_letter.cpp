e9ce1981a3a9b289608d5f402bc5005fcf6c12cb
NivedikaChoudhary <nivedikachoudhary2@gmail.com>
2026-03-18 16:03:38 +0530

    initial commit

---
+++ L29_queue/Day54/Q3_first_non_repeating_letter.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+void FirstNonRepeat(string str){ //O(n) O(n)
+    queue<char> Q;
+    int freq[26] = {0};
+
+    for(int i=0; i<str.size(); i++){
+        char ch = str[i];
+        Q.push(ch);
+        freq[ch-'a']++;
+
+        while(!Q.empty() && freq[Q.front()-'a'] > 1){
+            Q.pop();
+        }
+
+        if(Q.empty()){
+            cout<<"-1"<<endl;
+        }
+        else{
+            cout<<Q.front()<<endl;
+        }
+    }
+}
+
+int main(){
+    string str = "aabccxb";
+
+    FirstNonRepeat(str);
+    return 0;
+}
